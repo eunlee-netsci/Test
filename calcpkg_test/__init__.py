bd436b739a3d3deaa25c08a05c41820f095f5a80
Eun Lee <36894150+eunlee-netsci@users.noreply.github.com>
2023-12-18 19:03:31 +0900

    Add files via upload

---
+++ calcpkg_test/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+# 현재 calc 패키지의 operation, geometry 에서 모든 함수를 갖고 옴. 
+
+from .operation import add, multi,divide 
+from .geometry import triangle_area, rectangle_area 
+
+
+# _ # geometry 모듈에서 triangle_area, rectangle_area 함수를 바로 임포트. 
+
---
+++ calcpkg_test/geometry.py
@@ -0,0 +1,9 @@
+def triangle_area(base, height):
+    return base * height / 2
+ 
+def rectangle_area(width, height):
+    return width * height
+
+
+def rectangle_circum(width, height) : 
+    return width * 2 + height * 2 
